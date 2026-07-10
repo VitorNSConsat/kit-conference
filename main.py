ea6edf4b0b4cce34c2d621b2d72077c9b200b449
VitorNSConsat <vitor.consat@gmail.com>
2026-07-10 12:32:41 -0300

    fix: alterar porta para 8011

--- main.py
+++ main.py
@@ -370,4 +370,4 @@ async def reports(request: Request,
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8011, reload=True)
